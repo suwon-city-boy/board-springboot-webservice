3b852022dbc2ca0b1d1574f19cb4ab6f061dd737
suwon-city-boy <kimsungkyu.dev@gmail.com>
2021-02-08 22:45:25 +0900

    1. JpaRepository 테스트 코드 생성 2. Posts Entity 클래스 테스트 코드 추가

---
+++ src/test/java/com/scb/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.scb.study.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest //Default는 H2 database
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정하는 어노테이션
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글 제목";
+        String content = "테스트 게시글 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("kimsungkyu.dev@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
