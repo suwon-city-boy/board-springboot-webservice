df1129c59f3b633f239122e66c6b7542517f7f9d
suwon-city-boy <kimsungkyu.dev@gmail.com>
2021-02-22 23:11:30 +0900

    JPA Auditing 테스트 코드 작성   - BaseTimeEntity

--- src/test/java/com/scb/study/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/scb/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.scb.study.springboot.domain.posts;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -37,5 +41,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.of(2021, 2, 10, 0, 38, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build()
+        );
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
