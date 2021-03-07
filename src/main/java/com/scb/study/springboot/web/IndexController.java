b4daba012b83ab64df2a51feab7a767f833d0026
suwon-city-boy <kimsungkyu.dev@gmail.com>
2021-03-07 17:47:06 +0900

    오류 수정   - model에 담는 dto의 매핑 id 수정   - 'posts' -> 'post'

--- src/main/java/com/scb/study/springboot/web/IndexController.java
+++ src/main/java/com/scb/study/springboot/web/IndexController.java
@@ -1,19 +1,28 @@
 package com.scb.study.springboot.web;
 
+import com.scb.study.springboot.config.auth.dto.SessionUser;
 import com.scb.study.springboot.service.posts.PostsService;
 import com.scb.study.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private PostsService postsService;
+    private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null)
+            model.addAttribute("userName", user.getName());
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
@@ -26,7 +35,7 @@ public class IndexController {
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("posts", dto);
+        model.addAttribute("post", dto);
 
         return "posts-update";
     }
